77fa60a6b38e2fe75d68539a6d7644d5a96caeb9
Varin Nair <varinnair@gmail.com>
2018-06-05 14:19:31 -0700

    Added firebase functionality

--- ResourcesPage.js
+++ ResourcesPage.js
@@ -2,164 +2,91 @@ import React, { Component } from 'react';
 import { View, StyleSheet, ScrollView, TouchableOpacity, Text, Button } from 'react-native';
 import { Constants } from 'expo';
 
-// You can import from local files
-//import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
 import { Card } from 'react-native-elements'; // Version can be specified in package.json
 import { Actions } from 'react-native-router-flux';
+import firebase from 'firebase';
 
-export default class App extends Component {
-  render() {
-    return (
-      <ScrollView>
-        <View style={styles.container}>
-          <TouchableOpacity>
-            <Card title="" >
-            <View style={{justifyContent : "space-between", flexDirection: "row", borderBottomWidth: 1}}>
-            <Text style={{}}>TEEN VAN</Text>
-            <Text> 1.2 miles</Text>
-            </View>
-            <Text style={{marginBottom: 10}}>
-          Stanford Children's Health provides expert care for our community's high risk kids and young adults aged 10 to 25.
-          </Text>
-          <Button
-            icon={{name: 'code'}}
-            backgroundColor='#03A9F4'
-            fontFamily='Lato'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW'
-            onPress = {Actions.message}/>
-        </Card>
-          </TouchableOpacity>
+export default class SixteenTab extends Component {
 
-          <TouchableOpacity>
-            <Card title="" >
-            <View style={{justifyContent : "space-between", flexDirection: "row", borderBottomWidth: 1}}>
-            <Text style={{}}>TEEN VAN</Text>
-            <Text> 1.2 miles</Text>
-            </View>
-            <Text style={{marginBottom: 10}}>
-          Stanford Children's Health provides expert care for our community's high risk kids and young adults aged 10 to 25.
-          </Text>
-          <Button
-            icon={{name: 'code'}}
-            backgroundColor='#03A9F4'
-            fontFamily='Lato'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW'
-            onPress = {() => {console.log('varin')}}/>
-        </Card>
-          </TouchableOpacity>
+    constructor(props) {
+        super(props);
+        this.itemsRef = this.getRef();//.child('resources');
+        this.state = { items: null };
 
-          <TouchableOpacity>
-            <Card title="" >
-            <View style={{justifyContent : "space-between", flexDirection: "row", borderBottomWidth: 1}}>
-            <Text style={{}}>TEEN VAN</Text>
-            <Text> 1.2 miles</Text>
-            </View>
-            <Text style={{marginBottom: 10}}>
-          Stanford Children's Health provides expert care for our community's high risk kids and young adults aged 10 to 25.
-          </Text>
-          <Button
-            icon={{name: 'code'}}
-            backgroundColor='#03A9F4'
-            fontFamily='Lato'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW'
-            onPress = {() => {console.log('varin')}}/>
-        </Card>
-          </TouchableOpacity>
+    }
+    componentDidMount() {
+        this.listenForItems(this.itemsRef);
+    }
 
-          <TouchableOpacity>
-            <Card title="" >
-            <View style={{justifyContent : "space-between", flexDirection: "row", borderBottomWidth: 1}}>
-            <Text style={{}}>TEEN VAN</Text>
-            <Text> 1.2 miles</Text>
-            </View>
-            <Text style={{marginBottom: 10}}>
-          Stanford Children's Health provides expert care for our community's high risk kids and young adults aged 10 to 25.
-          </Text>
-          <Button
-            icon={{name: 'code'}}
-            backgroundColor='#03A9F4'
-            fontFamily='Lato'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW'
-            onPress = {() => {console.log('varin')}}/>
-        </Card>
-          </TouchableOpacity>
+    componentWillMount() {
+        this.setState({
 
-          <TouchableOpacity>
-            <Card title="" >
-            <View style={{justifyContent : "space-between", flexDirection: "row", borderBottomWidth: 1}}>
-            <Text style={{}}>TEEN VAN</Text>
-            <Text> 1.2 miles</Text>
-            </View>
-            <Text style={{marginBottom: 10}}>
-          Stanford Children's Health provides expert care for our community's high risk kids and young adults aged 10 to 25.
-          </Text>
-          <Button
-            icon={{name: 'code'}}
-            backgroundColor='#03A9F4'
-            fontFamily='Lato'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW'
-            onPress = {() => {console.log('varin')}}/>
-        </Card>
-          </TouchableOpacity>
+            items: []
+        });
+    }
 
-          <TouchableOpacity>
-            <Card title="" >
-            <View style={{justifyContent : "space-between", flexDirection: "row", borderBottomWidth: 1}}>
-            <Text style={{}}>TEEN VAN</Text>
-            <Text> 1.2 miles</Text>
-            </View>
-            <Text style={{marginBottom: 10}}>
-          Stanford Children's Health provides expert care for our community's high risk kids and young adults aged 10 to 25.
-          </Text>
-          <Button
-            icon={{name: 'code'}}
-            backgroundColor='#03A9F4'
-            fontFamily='Lato'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW'
-            onPress = {() => {console.log('varin')}}/>
-        </Card>
-          </TouchableOpacity>
+    getRef() {
+        return firebase.database().ref('/resources');
+    }
 
-          <TouchableOpacity>
-            <Card title="" >
-            <View style={{justifyContent : "space-between", flexDirection: "row", borderBottomWidth: 1}}>
-            <Text style={{}}>TEEN VAN</Text>
-            <Text> 1.2 miles</Text>
-            </View>
-            <Text style={{marginBottom: 10}}>
-          Stanford Children's Health provides expert care for our community's high risk kids and young adults aged 10 to 25.
-          </Text>
-          <Button
-            icon={{name: 'code'}}
-            backgroundColor='#03A9F4'
-            fontFamily='Lato'
-            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-            title='VIEW NOW'
-            onPress = {() => {console.log('varin')}}/>
-        </Card>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    );
-  }
+    listenForItems(resourcesRef) {
+        resourcesRef.on('value', (snap) => {
+            var resources = [];
+            snap.forEach((child) => {
+                resources.push({
+                    name: child.val().name,
+                    location: child.val().location,
+                    description: child.val().description,
+                    lat: child.val().lat,
+                    long: child.val().long,
+                    key: child.key
+                });
+            });
+            this.setState({
+                items: resources
+            });
+        });
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <View style={styles.container}>
+                    {
+                        this.state.items.map((item, index) => {
+                            return (
+                                <TouchableOpacity key={index}>
+                                    <Card title="" >
+                                        <View style={{ justifyContent: "space-between", flexDirection: "row", borderBottomWidth: 1 }}>
+                                            <Text style={{}}>{item.name}</Text>
+                                            <Text>{item.location}</Text>
+                                        </View>
+                                        <Text style={{ marginBottom: 10 }}>
+                                            {item.description}
+                                        </Text>
+                                        <Button
+                                            icon={{ name: 'code' }}
+                                            backgroundColor='#03A9F4'
+                                            fontFamily='Lato'
+                                            buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+                                            title='VIEW NOW'
+                                            onPress={Actions.maps} />
+                                    </Card>
+                                </TouchableOpacity>
+                            )
+                        })
+                    }
+                </View>
+            </ScrollView>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-
-    //alignItems: 'center',
-    //justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#ecf0f1',
+    },
 });
